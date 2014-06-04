ac0abcd8db7be6c4b820e69982083f026f2e0f26
G Brookshire <broog731@newschool.edu>
2014-06-03 19:59:40 -0500

    Make the model_list.
    A list of things about each term in the model. Also made the numeric
    part of the E(MS) table.

--- ems.py
+++ ems.py
@@ -9,75 +9,76 @@ Tom
 import re
 
 class ExMeanSquare:
-    def __init__(self, model, fx_type):
-        # Takes 2 arguments: a linear model in latex form and
-        # a list containing a 'fixed' or 'random' entry for each model term
+    def __init__(self, model, effect_type, n_levels):
+        """ Args:
+            model: a linear model in latex form
+                E.g. r"\alpha_{j} + \beta_{k} + \pi_{i(j)} + \epsilon_{i(jk)}"
+                Be sure to use a raw string!
+            effect_type: a list of 'fixed' or 'random' for each model term.
+            n_levels: a list of number of levels in each non-error term.
+                len(n_levels) must equal len(model) - 1, since we don't
+                need the number of levels of the error term.
+        """
 
         # 1 - Write linear model
-        # This is a latex model of the parameters of interest, of the form:
-        # alpha_{j} + beta_{k} + pi_{i(j)} + epsilon_{i(jk)}
-        # Be sure to use a raw string -- r"\alpha + \whatever"
-        print model
-
-        # make a dictionary to store info for each model term
-        model_dict = {}
-
-        # split up the model into each of the terms
+        # Error checking
         model_terms = [s.strip() for s in model.split('+')]
+        if any([e not in 'fixed random' for e in effect_type]):
+            raise(Exception('effect_type must be a list of "fixed"/"random".'))
+        if not len(model_terms) == len(effect_type) == (len(n_levels) + 1):
+            raise(Exception('Args must have the same length.'))
 
         # get the subcomponents of each term
-        i = 0
-        for term in model_terms:
-            # create index to refer to model terms
-            term_number = 'term_' + str(i+1)
+        model_list = []
+        # i = 0
+        for i_term, term in enumerate(model_terms):
             # get the factor name
-            factor_name = term[:term.find('_')] 
+            factor_name = term[:term.index('_')] 
             # get all subscripts for a factor
             factor_sub = re.findall('{(.+?)}', term)
             # split subscripts into whether they're in parenthesis
             factor_sub = factor_sub[0].split('(')
             # get the subscripts not in parenthesis
-            factor_sub_no_par = list(factor_sub[0])
+            factor_sub_no_par = factor_sub[0]
             # get the subscripts in parenthesis if applicable
             if len(factor_sub) > 1:
-                factor_sub_par = list(factor_sub[1].replace(')', ''))
+                factor_sub_par = factor_sub[1].replace(')', '')
             elif len(factor_sub) == 1:
-                factor_sub_par = ['']
-
-            # add everything to the dictionary
-            model_dict[term_number] = {'name': factor_name,
-                                        'subscripts_nopar': factor_sub_no_par,
-                                        'subscripts_par': factor_sub_par,
-                                        'effect_type': fix_list[i]}
-            i +=1
+                factor_sub_par = ''
+            # Get the number of levels
+            if i_term == len(n_levels):
+                lev = None
+            else:
+                lev = n_levels[i_term]
+            term_dict = {'name': factor_name,
+                         'n_levels': lev,
+                         'subscripts_nopar': factor_sub_no_par,
+                         'subscripts_par': factor_sub_par,
+                         'effect_type': effect_type[i_term]}
+            model_list.append(term_dict)
 
         # 2 - Construct a 2-way table
-        # Table constructed from list of lists.
-        # Rows: each term in the model
-        # Cols: each subscript in the model
-        rownames = [s.strip() for s in model.split('+')]
-              
-        # get all the subscripts
-        all_subscripts = []
-        # extract the subscripts for each term
-        for key in model_dict.keys():
-            all_subscripts.extend(model_dict[key]['subscripts_nopar'])
-            all_subscripts.extend(model_dict[key]['subscripts_par'])
+        # Table constructed from list of lists. Initialized as list of Nones.
+        # Rows: each term of the model
+        # Cols: each subscript present in the model
+        rownames = model_terms
+        colnames = set([term['subscripts_nopar'] for term in model_list])
+        table = [[None for n in range(len(colnames))] \
+                    for m in range(len(rownames))]
 
-        # get rid of empty strings
-        all_subscripts = filter(None, all_subscripts)
-        # reduce to unique subscripts and sort them
-        sorted(set(all_subscripts))
-
-        # set the colnames
-        colnames = all_subscripts
-
-        table = [[None] * len(colnames)] * len(rownames)
-
-        # 3 - Fill in the cells of the table
+        # 3 - Fill in the numeric cells of the table
         for i_col, subscript in enumerate(colnames):
             for i_row, param in enumerate(rownames):
                 cell = 'FILL_IN'
+                if re.search('\(?.*' + subscript + '.*\)?', param): # inside ()
+                    cell = 1
+                elif re.search(subscript + '.*\(', param): # outside ()
+                    if model_list[i_row]['effect_type'] in 'fixed':
+                        cell = 1
+                    elif model_list[i_row]['effect_type'] in 'random':
+                        cell = 0
+                elif not re.search(subscript, param): # not in term
+                        cell = model_list[i_row]['n_levels']
                 table[i_row][i_col] = cell
 
         # 4 - blah blah
@@ -85,6 +86,9 @@ class ExMeanSquare:
         # 5 - blah blah
 
         self.model = model
+        self.model_list = model_list
+        self.rownames = rownames
+        self.colnames = colnames
         self.table = table
 
     def latex_table(self):
